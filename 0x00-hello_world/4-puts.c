1297b11c2d5c35311cae4b5e99ea797c96ad0e44
Chol Paul <cholthipaul@gmail.com>
2023-02-17 22:33:36 -0800

    C program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*output string using puts stdio function*/
-int main(void) 
-{ 
+/* output string using puts stdio function */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

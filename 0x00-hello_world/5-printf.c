1bb32dae57d685e2e38897e43e192aaa623b1191
Chol Paul <cholthipaul@gmail.com>
2023-02-17 23:01:29 -0800

    C program using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 char *str;
-str="with proper grammar, but the outcome is a piece of art,";
-printf("%s\n",str);
+str = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", str);
 return (0);
 }

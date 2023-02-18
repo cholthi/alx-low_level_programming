b782e8b61d4d68a33f366d2641f25e8f2803f522
Chol Paul <cholthipaul@gmail.com>
2023-02-17 22:57:57 -0800

    C program using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - output string using printf stdio function
+*
+* Description: outputs string with printf
+* Return: 0
+*/
+int main(void)
+{
+char * str;
+str="with proper grammar, but the outcome is a piece of art,";
+printf("%s\n",str);
+return (0);
+}

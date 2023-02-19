8b730dfe5f442bd02f7b154dd5d152a35c2c3bb9
Chol Paul <cholthipaul@gmail.com>
2023-02-19 12:21:05 -0800

    Print the alphabets using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - Print the  alphabetfollowed by newline
+*
+* Description: Print the alphabet followed by newline
+* Return: int
+*/
+int main(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		putchar(start);
+	}
+
+	for (start = 'A'; start <= 'Z'; start++)
+        {
+                putchar(start);
+        }
+	putchar('\n');
+	return (0);
+}

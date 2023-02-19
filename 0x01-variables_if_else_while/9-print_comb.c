8ab99fca080cafbfb42e7a96ead75bfdbb579509
Chol Paul <cholthipaul@gmail.com>
2023-02-19 13:32:31 -0800

    print out formated numbers using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* main - Print the numbers 0 to 9
+*
+* Description: Print the numbers 0 to 9
+* Return: int
+*/
+int main(void)
+{
+	int start;
+
+	for (start = 0; start <= 9; start++)
+	{
+		if (start != 9)
+		{
+			putchar('0' + start);
+			putchar(',');
+			putchar(' ');
+		}else
+		{
+			putchar('0' + start);
+		}
+	}
+	printf("\n");
+	return (0);
+}

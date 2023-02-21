479a07599d805a21311e864b4aaae8791afd4a80
Chol Paul <cholthipaul@gmail.com>
2023-02-21 14:02:46 -0800

    check the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,17 @@
 /* more headers goes there */
 
 /**
-* _isalpha - checks the sign of a number
+* print_sign - checks the sign of a number
 * @c: Int char to check
 *
 * Description: Checksthe sign of a number
 * Return: int
 */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	int sign
+	int sign;
 	
-	sign = (x > 0) - (x < 0);
+	sign = (n > 0) - (n < 0);
 	if (c == 0)
 	{
 		_putchar('0');

26986df16cea362c6212c9bcbb9353ca084be9b3
Chol Paul <cholthipaul@gmail.com>
2023-02-21 14:21:00 -0800

    computes absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _abs - computes absolute value of a number
+* @n: Int char to check
+*
+* Description: Computes absolute value of a number
+* Return: int bool
+*/
+int _abs(int n)
+{
+	int mask = (n >> 31);
+
+	return (mask ^ n) - mask;
+}
+

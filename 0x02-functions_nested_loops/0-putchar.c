f42f5241b929b178b5726560f12080651f6cbe63
Chol Paul <cholthipaul@gmail.com>
2023-02-21 11:00:05 -0800

    print a string without using std lib functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,12 @@
 * Description: Print the _putchar
 * Return: int
 */
+
 int main(void)
 {
 	char str[] = "_putchar";
 	int i;
-	for(i=0; i <=7; i++)
+	for (i=0; i <=7; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 

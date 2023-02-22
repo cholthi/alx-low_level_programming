fe2fc1456d5842f50c575a46da1edad09eb5b2af
Chol Paul <cholthipaul@gmail.com>
2023-02-22 02:14:16 -0800

    print the nines times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* times_table - print the 9 times table
+*
+* Description: print the nines times table
+* Return: void
+*/
+void times_table(void)
+{
+	int x;
+	int y;
+	int table;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			table = y * x;
+			if (table > 10)
+			{
+				_putchar((table / 10) + '0');
+				_putchar((table % 10) + '0');
+			} else
+			{
+				_putchar(table + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+
+		}
+		_putchar('\n');
+	}
+}
+

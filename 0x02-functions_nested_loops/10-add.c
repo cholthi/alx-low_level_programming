d48b261e5243854d9cf8d49daeda06bb1fe21574
Chol Paul <cholthipaul@gmail.com>
2023-02-22 04:07:57 -0800

    add two numbers and print the results

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -32,8 +32,11 @@ void print_digits(long int n)
 		_putchar('-');
 		n = -n;
 	}
+	if (n == 0)
+		_putchar('0');
 
 	if (n / 10)
 		print_digits(n / 10);
-	_putchar((n % 10) + '0');
+
+	_putchar(n % 10 + '0');
 }

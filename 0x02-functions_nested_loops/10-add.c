e8ac9d96be3ed70488981a8b600ee913b4f3c3ce
Chol Paul <cholthipaul@gmail.com>
2023-02-22 03:47:29 -0800

    add tow numbers and print the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,11 @@
 /* more headers goes there */
 
 /**
-* _abs - computes product of two numbers
+* add - computes sum of two numbers
 * @n: Int number to add
 * @n: number to add
 *
-* Description: Computes product of two numbers
+* Description: Computes sum of two numbers
 * Return: int
 */
 int add(int a, int b)

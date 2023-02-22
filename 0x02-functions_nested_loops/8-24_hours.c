1983d87a4d5b4f6c23f376b9af300c6da7cca69b
Chol Paul <cholthipaul@gmail.com>
2023-02-22 01:35:40 -0800

    print Jack Bauer operation time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* jack_bauer - Print jack bauer day minutes
+*
+* Description: Print jack bauer day minutes
+* Return: void
+*/
+int jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 0; minutes++)
+		{
+			if (minutes < 10)
+				_putchar(minutes + '0');
+			else
+			{
+				_putchar((minutes / 10)+'0');
+				_putchar((minutes % 10)+'0');
+			}
+			_putchar(':');
+			if (hours < 10)
+				_putchar(hours + '0');
+			else
+			{
+				_putchar((hours / 10)+'0');
+				_putchar((hours % 10)+'0');
+			}
+			_putchar('\n');
+		}
+	}
+}
+

3d3b6d04a1c1ddf5bb5b240054e983a4e602b874
Chol Paul <cholthipaul@gmail.com>
2023-02-24 00:24:46 -0800

    print 1 to 14 ten times and newline

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,8 +16,28 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
-			_putchar('\n');
+			print_digits(i);
 		}
+		_putchar('\n');
 	}
+}
+
+/**
+* print_digits - computes product of two numbers
+* @n: Int number to print
+*
+* Description: Computes product of two numbers
+* Return: void
+*/
+void print_digits(int n)
+{
+        if (n < 0)
+        {
+                _putchar('-');
+                n = n * -1;
+        }
+
+        if (n / 10)
+                print_digits(n / 10);
+        _putchar(n % 10 + '0');
 }

d69117a00f0ec4d9c85fe26b4b410ad4e8922945
Chol Paul <cholthipaul@gmail.com>
2023-02-23 22:49:42 -0800

    check if uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,13 @@
 
 /**
 * _abs - checks its argument if it is a uppercase character
-* @n: Int char to check
+* @c: Int char to check
 *
 * Description: Check argument if uppercase
 * Return: int bool
 */
 int _isupper(int c)
 {
-	int isupper = (c >= 65 && c <= 90);
-	return (isupper);
+	return ((c >= 65 && c <= 90));
 }
 

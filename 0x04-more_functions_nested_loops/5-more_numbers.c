3ef5763b2f03b9525414279ada47f5084beae16c
Chol Paul <cholthipaul@gmail.com>
2023-02-24 00:14:11 -0800

    print 1 to 14 ten times and newline

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* more_numbers - print 1 to 14  ten times followed by a newline
+*
+* Description: Print 1 to 14 ten times followed by a newline
+* Return: void
+*/
+void more_numbers(void)
+{
+	int ;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+			_putchar('\n');
+		}
+	}
+}

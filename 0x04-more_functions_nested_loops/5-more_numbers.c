e34ed5b929528987e51fd3113e5cc783fbbde42e
Chol Paul <cholthipaul@gmail.com>
2023-02-24 00:28:58 -0800

    print 1 to 14 ten times and newline

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -31,13 +31,13 @@ void more_numbers(void)
 */
 void print_digits(int n)
 {
-        if (n < 0)
-        {
-                _putchar('-');
-                n = n * -1;
-        }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
 
-        if (n / 10)
-                print_digits(n / 10);
-        _putchar(n % 10 + '0');
+	if (n / 10)
+	print_digits(n / 10);
+	_putchar(n % 10 + '0');
 }

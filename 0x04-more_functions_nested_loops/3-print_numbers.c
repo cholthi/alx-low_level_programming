c690a20068708b1d993ff94a7c9206a26fd96ac2
Chol Paul <cholthipaul@gmail.com>
2023-02-23 23:34:14 -0800

    print 1 to 9 and newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_numbers - print 1 to 9 followed by a newline
+*
+* Description: Print 1 to 9 followed by a newline
+* Return: void
+*/
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
+

c2b9050126cd9653a63f82de5cb741e8b8d508cd
Chol Paul <cholthipaul@gmail.com>
2023-02-23 23:14:34 -0800

    check if uppercase character

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _isdigit - checks its argument if it is a numeric digit character
+* @c: Int char to check
+*
+* Description: Check argument if numeric digit
+* Return: int bool
+*/
+int _isdigit(int c)
+{
+	return ((c >= 48 && c <= 57));
+}
+

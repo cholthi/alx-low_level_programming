bb74cd99b9c0a49e023daa9b09da95804ee26ef9
Chol Paul <cholthipaul@gmail.com>
2023-02-23 23:19:40 -0800

    multiple arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* mul - checks its argument if it is a uppercase character
+* @a: Int char to check
+* @b: Int argment to mult
+*
+* Description: Check argument if uppercase
+* Return: int result
+*/
+int mul(int a, int b)
+{
+	long int r
+
+	r = a * b;
+	return (r);
+}
+

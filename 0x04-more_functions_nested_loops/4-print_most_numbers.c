d051be8665ab3e6f41abaab3c7cc734ce70ad51f
Chol Paul <cholthipaul@gmail.com>
2023-02-23 23:58:12 -0800

    print 1 to 9 without 2 and 4 and newline

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_most_numbers - print 1 to 9 followed by a newline
+*
+* Description: Print 1 to 9 followed by a newline
+* Return: void
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}

df8da82d3e664436b243ffd3aa032015cade57f9
Chol Paul <cholthipaul@gmail.com>
2023-02-24 00:43:50 -0800

    print ascii line according to length  and  add newline

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,13 @@ void print_line(int n)
 {
 	int i;
         
-	if (n == 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
+		return ();
 	}
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		_putchar('_');
 	}

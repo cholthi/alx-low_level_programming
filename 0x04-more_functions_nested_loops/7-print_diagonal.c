7676acf54832ada357f679f2bd0abecca56f2a48
Chol Paul <cholthipaul@gmail.com>
2023-02-25 23:20:07 -0800

    print diagonal according to legth argument

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_diagonal - print ascii diagonal according to argument length followed
+* @n: int line length
+*
+* Description: print ascii diagonal according to argument length follow
+* Return: void
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int ident;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (ident = 0; ident < i; ident++)
+			_putchar(' ');
+
+		_putchar('\');
+		_putchar('\n');
+		
+	}
+}

37f83868b05539264acb70f77a70a2eb587f2f42
Chol Paul <cholthipaul@gmail.com>
2023-02-28 23:13:14 -0800

    print n elements o an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /* more headers goes there */
 
 /**
@@ -14,28 +15,7 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n -1; i++)
-		print_digits(a[i]);
+		printf("%d", a[i]);
 
 	_putchar('\n');
-}
-
-
-/**
-* print_digits - computes product of two numbers
-* @n: Int number to print
-*
-* Description: Computes product of two numbers
-* Return: void
-*/
-void print_digits(int n)
-{
-	if (n < 0)
-	{
-		 _putchar('-');
-		n = n * -1;
-	}
-
-	if (n / 10)
-		print_digits(n / 10);
-	 _putchar(n % 10 + '0');
 }

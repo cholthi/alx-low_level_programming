cadec62401c19a9351a22ba8db287cfe1454209d
Chol Paul <cholthipaul@gmail.com>
2023-02-28 22:41:48 -0800

    print last halve of a C null terminated C string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -31,16 +31,16 @@ void puts_half(char *str)
 
 /**
 * _strlen - get the length of a string given to it
-* sn: *s char string to calculate length in bytes
+* @s: *s char string to calculate length in bytes
 *
 * Description: get the length of a string given to it
 * Return: int
 */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*(s + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);

39290c46c581c20f352daf0a3d84f2d5bb2a0510
Chol Paul <cholthipaul@gmail.com>
2023-03-01 00:06:09 -0800

    std lib strcpy clone

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,8 @@
 
 /**
 * _strcpy - get the length of a string given to it
-* @dest: *char dest string
-* @src: * char src string
+* dest: *char dest string
+* *src: * char src string
 *
 * Description: get the length of a string given to it
 * Return: int
@@ -12,11 +12,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	
-	while ( src[i] != '\0')
+
+	while (src[i] != '\0')
 	{
 		*(dest + i) = *src + i;
+		i++;
 	}
+	dest[i] = '\0';
+
 
 	return (src);
 }

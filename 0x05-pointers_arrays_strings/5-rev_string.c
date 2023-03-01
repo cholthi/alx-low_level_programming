d3cc958e3c64d02894c0eb97399ce45c8d315262
Chol Paul <cholthipaul@gmail.com>
2023-02-28 21:48:39 -0800

    reverse a pointer to charater in place

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,14 +14,13 @@ void rev_string(char *s)
 	char *begin, *end, temp;
 
 	length = _strlen(s);
- 
+
 	begin = s;
 	end = s;
-
-	for ( c = 0 ; c < ( length - 1 ) ; c++ )
+	for (c = 0 ; c < ( length - 1 ) ; c++)
 		end++;
 
-	for ( c = 0 ; c < length/2 ; c++ ) 
+	for ( c = 0 ; c < length/2 ; c++ )
 	{        
 		temp = *end;
 		*end = *begin;

c9b38fcdfc9c8bce6d1f8b472c66ff9ed72b7fe6
Chol Paul <cholthipaul@gmail.com>
2023-02-28 22:07:31 -0800

    print every other character of a C null terminated C string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 /* more headers goes there */
 
 /**
-* puts2 print every other of a string
+* puts2 - print every other of a string
 * @str: * strint to print
 *
 * Description: print every other charatcer of a string
@@ -11,9 +11,27 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	for ( i = 0 ; str[i] != '\0' ; i += 2 )
+	for (i = 0; i <= _strlen(str); i += 2)
 		_putchar(str[i]);
 
 	_putchar('\n');
+}
+
+/**
+* _strlen - get the length of a string given to it
+* @s: * char string to calculate length in bytes
+*
+* Description: get the length of a string given to it
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }

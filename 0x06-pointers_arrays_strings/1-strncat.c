be5e5836db0d7db3b0e8e352956bb4ec8facf2f6
Chol Paul <cholthipaul@gmail.com>
2023-03-01 23:18:52 -0800

    concateneats two C string using a length

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _strncat - concatnates two strings
+* @dest: * char destination string
+* @src: * char source string
+* @n: int number of bytes to copy
+*
+* Description: get the length of a string given to it
+* Return: * char
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int len;
+	int j;
+
+	len = _strlen(dest);
+	for (j = 0; src[j] < n; j++, len++)
+	{
+		dest[len] = src[j];
+	}
+	dest[len] = '\0';
+
+	return (dest);
+
+}
+
+/**
+* _strlen - get the length of a string given to it
+* @s: * char string to calculate length in bytes
+*
+* Description: get the length of a string given to it
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+

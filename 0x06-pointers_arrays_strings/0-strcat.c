c798536b97a709c0fa21c44662ccdafeb0a49515
Chol Paul <cholthipaul@gmail.com>
2023-03-01 21:59:00 -0800

    cat two C strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _strcat - concatnates two strings
+* @dest: * char destination string
+* @src: * char source string
+*
+* Description: get the length of a string given to it
+* Return: int
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len;
+	int j;
+
+	len = _strlen(dest);
+
+	for (j = 0; src[j] != '\0'; j++, len++)
+	{
+		dest[len] = src[j];
+		len++;
+		j++;
+	}
+	dest[len] = '\0';
+
+	return dest;
+
+}
+
+/**
+* _strlen - get the length of a string given to it
+* @s: * char string to calculate length in bytes
+*
+* Description: get the length of a string given to it
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+

2698b5e5c74189a84eab4a0ddcd8651c95cc2caf
Chol Paul <cholthipaul@gmail.com>
2023-03-02 15:26:38 -0800

    to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* string_toupper - changes character of a given string to upper case
+* @s: * char string
+*
+* Description: changes character of a given string to upper case
+* Return: * char
+*/
+char *string_toupper(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s = *s - 32;
+		s++;
+	}
+}
+

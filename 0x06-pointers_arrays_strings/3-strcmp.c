9c86a9f54c252da20c2a0c63d3843c657ac064f5
Chol Paul <cholthipaul@gmail.com>
2023-03-02 14:31:13 -0800

    compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _strcmp - compares two strings lexigraphically
+* @s1: * char string one
+* @s2: * char string two
+*
+* Description: compares two strings lexigraphically
+* Return: int
+*/
+int _strcmp(char *s1, char *s2);
+{
+
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*(const unsigned char *) s1 - *(const unsigned char *) s2);
+}

0f2fd4b2a2517fcf35e37119ab44844be2d9fc44
Chol Paul <cholthipaul@gmail.com>
2023-03-06 12:11:53 -0800

    searches for needle in the haystack

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strchr - search needle in the haystack
+ * @s: * char haystack
+ * @c: char needle
+ *
+ * Return: * char returns pointer to first occurence of needle or null
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (i != '\0')
+	{
+		if (c == s[i]
+			return (s[i]);
+		i++;
+	}
+
+	return ('\0');
+}

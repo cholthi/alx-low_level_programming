623d628e20f2bb84ab054fca2a14a101e6aa6bc0
Chol Paul <cholthipaul@gmail.com>
2023-03-06 14:41:00 -0800

    print two matrix diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * _strspn - prints the sum of the two diagonals of a square matrix of integers.
+ * @a: * int matrix of int
+ * @n:int matrix size
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s1 = 0;
+	int s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		s2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", s1, s2);
+}

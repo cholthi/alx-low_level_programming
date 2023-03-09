5030b978d53ee7217d111c50dcda186622fb83d9
Chol Paul <cholthipaul@gmail.com>
2023-03-09 11:19:48 -0800

    strlen using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		len += 1;
+		return (0);
 	}
 
-	return len;
+	return (_strlen_recursion(s + 1) + 1);
 }

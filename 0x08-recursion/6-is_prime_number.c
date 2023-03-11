37181ae4de63888381ca9017a9229f28d5788874
Chol Paul <cholthipaul@gmail.com>
2023-03-10 23:04:15 -0800

    check number is prime using recusrsive method

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,19 @@
  */
 int check_prime(int a, int b)
 {
-	if(a < 2)
+	int res;
+
+	if (a < 2)
 		return (0);
-	if (a == 2 || a==3)
+	if (a == 2 || a == 3)
 		return (1);
 	if (a % b == 0)
 		return (0);
 	if (a <= a / 2)
 		return (1);
 
-	return (check_prime(a, b + 1));
+	res = check_prime(a, b + 1);
+	return (res);
 }
 
 /**

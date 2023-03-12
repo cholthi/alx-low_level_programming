f818fd948f8ef347d48ce189d59981e47470714d
Chol Paul <cholthipaul@gmail.com>
2023-03-11 22:05:10 -0800

    print the number of arguments passed to the program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+* print_number - print C string aka, pointernto character
+* @n: int number to print
+*
+* Description: print C integer as a decimal ascii chaaracters
+* Return: void
+*/
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
+
+/**
+ * main - print the number of aguments passed to it
+ * @argc: int arguments count
+ * @argv: * char[] arrray of strings
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	print_number(argc);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,3 @@
 void _puts(char *str);
 void _putchar(char c);
+void print_number(int n);

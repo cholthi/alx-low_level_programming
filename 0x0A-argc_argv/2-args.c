32c99b61f5f50ca8adc2bdddb0b5c537fbd9d158
Chol Paul <cholthipaul@gmail.com>
2023-03-11 22:18:54 -0800

    print the arguments passed to the program

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+* _puts - print C string aka, pointernto character
+* @str: *char char to print
+*
+* Description: print C string aka, pointernto character
+* Return: void
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	 _putchar('\n');
+}
+
+/**
+ * main - print the program arguments
+ * @argc: int arguments count
+ * @argv: * char[] arrray of strings
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int counter;
+	if (argc > 0)
+	{
+		for (counter = 0; counter < argc; counter++)
+			_puts(argv[counter]);
+	}
+	return (0);
+}

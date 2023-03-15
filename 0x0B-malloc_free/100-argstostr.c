7bc010ab36a7673b8f9505be8402808ba2fc0b3b
Chol Paul <cholthipaul@gmail.com>
2023-03-15 15:24:10 -0700

    concats elements of char int to newline sep string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * argtostr - concats elements of array to a single string
+ * @ac: int number of elems in the array
+ * @height: char ** 2d array of chars
+ *
+ * Return: char *
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, i, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[i++] = av[arg][byte];
+
+		str[i++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}

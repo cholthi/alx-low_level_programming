de483a026009e2b99d2c0f4efd1378ff7e962f11
Chol Paul <cholthipaul@gmail.com>
2023-03-15 14:50:22 -0700

    dynamic 2d array using double pointers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,9 +19,15 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	arr = (int **) malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *) malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+
 	}
 
 	for (i = 0; i < height; i++)

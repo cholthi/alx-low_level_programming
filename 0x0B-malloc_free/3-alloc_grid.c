27920cc6aedf8cc0a878fe901d87c2c61666200a
Chol Paul <cholthipaul@gmail.com>
2023-03-15 14:34:43 -0700

    dynamic 2d array using double pointers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - dynamically create 2d array of ints
+ * @width: array columns
+ * @height: array rows
+ *
+ * Return: ** int 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i;
+	int j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	arr = (int **) malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *) malloc(width * sizeof(int));
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}

cad376c75a409ae4a344446d5d39221bd0bb4e99
Chol Paul <cholthipaul@gmail.com>
2023-03-26 01:46:36 -0700

    0x0D-processor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef SIZE_H
+#define SIZE_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif

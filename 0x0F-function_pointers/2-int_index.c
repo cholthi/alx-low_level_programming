a45ed5c423daa21ef38fdf9521ebd6e6586c9d54
Chol Paul <cholthipaul@gmail.com>
2023-03-29 00:00:29 -0700

    searches an array and returns the matching index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/*
+ * int_index - searches an array for an element and returns thematching index
+ * @array: * int array
+ * @size: size of the above array
+ * @cmp: pointer to function that takes int and returns int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -12,3 +12,5 @@ void print_name(char *name, void (*f)(char *));
 void _putchar( char);
 void (*action)(int);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int (*cmp)(int);
+int int_index(int *array, int size, int (*cmp)(int));

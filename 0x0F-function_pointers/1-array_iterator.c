30a4017287f3ff78729ad41f13c9a20784185f85
Chol Paul <cholthipaul@gmail.com>
2023-03-28 23:45:03 -0700

    walks an array using a callback function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/*
+ * array_iterator - Iterates over array sending each value to action callback
+ * @array: * int array
+ * @size: size of the above array
+ * @action: pointer to function callback
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,3 +10,5 @@
 void (*f)(char *);
 void print_name(char *name, void (*f)(char *));
 void _putchar( char);
+void (*action)(int);
+void array_iterator(int *array, size_t size, void (*action)(int));

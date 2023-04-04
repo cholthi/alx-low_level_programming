0498fdb8abbe9587dedd939f8d905dda8ce2ac71
Chol Paul <cholthipaul@gmail.com>
2023-04-04 16:22:55 -0700

    add at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *new, *prev;
+	listint_t *current, *new;
 	size_t count;
 
 	new = malloc(sizeof(listint_t));

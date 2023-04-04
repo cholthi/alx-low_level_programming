f804729e63382795b617a1ca23e553c89d6079dd
Chol Paul <cholthipaul@gmail.com>
2023-04-04 14:03:37 -0700

    free linked list 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * free_listint2 - Free the memory for linked list 2
+ * @head: First node of the linked list or head node
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
+

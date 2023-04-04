cc4757b8b5263c7c34ca2842f2bc1e53782d1e95
Chol Paul <cholthipaul@gmail.com>
2023-04-04 11:52:09 -0700

    Display linked list nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - Prints nodes of a linked list
+ * @h: First node of the linked list or head node
+ *
+ * Return: size_it number nodes  in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+
+		len++;
+	}
+
+	return (len);
+}

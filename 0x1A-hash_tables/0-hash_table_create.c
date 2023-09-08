7878b9a836d74c1dd36ed851abed3419690696be
Chol Paul <cholthipaul@gmail.com>
2023-09-08 13:18:33 -0700

    correction for 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
+	unsigned long int i;
+
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht.size = size;
+	ht->size = size;
 
-	//Allocate initialized hashtable Buckets hash_node_t
+	/*Allocate initialized hashtable Buckets hash_node_t*/
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;	
+		ht->array[i] = NULL;
 	return (ht);
 
 }
+

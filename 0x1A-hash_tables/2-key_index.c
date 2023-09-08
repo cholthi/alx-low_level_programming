d591ca8eebd32c109d46b84c9a9602d00bef4894
Chol Paul <cholthipaul@gmail.com>
2023-09-08 13:33:44 -0700

    correction for gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 1-djb2.c 2-key_index.c -o c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * key_index - Returns the index of the hash_node_t key
  * @key: The hash_node_t key to hash
@@ -11,8 +13,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hash;
 
 	hash = hash_djb2(key);
-	index = (unsigned long int) hash & (unsigned long int)(size - 1);
+	index = (unsigned long int) (hash % size);
 
 	return (index);
-	
 }
+

8b43f1be763854787c10a3833dab3421e1e9f916
Chol Paul <cholthipaul@gmail.com>
2023-09-08 14:04:40 -0700

    correction for 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * hash_table_get - Get a vlaue at key from hash table
  * @ht: Hash table struct
@@ -10,13 +12,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node = NULL;
 
-	index = key_index(key, ht->size);
+	index = key_index((unsigned const char *)key, ht->size);
 	node = ht->array[index];
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key))
-			return (node->data);
-		node = node->next
+			return (node->value);
+		node = node->next;
 	}
 	return (NULL);
 }
